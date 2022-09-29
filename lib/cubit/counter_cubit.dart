64c4a2125887242221fee0347b5cc839b025caaa
MusabBoltX <musabsyed714@gmail.com>
2022-09-29 15:31:44 +0500

    Counter With Bloc Builder

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -6,7 +6,23 @@ part 'counter_state.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState(counter: 0));
 
-  void increment() => emit(CounterState(counter: state.counter + 1));
+  void increment() =>
+      emit(CounterState(counter: handleCounterIncrement(state.counter)));
 
-  void decrement() => emit(CounterState(counter: state.counter - 1));
+  void decrement() =>
+      emit(CounterState(counter: handleCounterDecrement(state.counter)));
+
+  int handleCounterIncrement(count) {
+    count = count + 1;
+    return count;
+  }
+
+  int handleCounterDecrement(count) {
+    if (count == 0) {
+      return 0;
+    } else {
+      count = count - 1;
+      return count;
+    }
+  }
 }
--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -12,37 +12,37 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Buckle Up With Bloc!"),
       ),
-      body: BlocBuilder<CounterCubit, CounterState>(
-        builder: (context, state) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(
                   '${state.counter}',
                   style: Theme.of(context).textTheme.headline4,
-                ),
-              ],
+                );
+              },
             ),
-          );
-        },
+          ],
+        ),
       ),
       bottomNavigationBar: Row(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           FloatingActionButton(
-            onPressed: () => context.read<CounterCubit>().increment(),
-            tooltip: 'Increment Counter',
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
             onPressed: () => context.read<CounterCubit>().decrement(),
             tooltip: 'Decrement Counter',
             child: const Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: () => context.read<CounterCubit>().increment(),
+            tooltip: 'Increment Counter',
+            child: const Icon(Icons.add),
           ),
         ],
       ),
